373866d3e4e1657c59d01d0d51ac7a0c69e6bb0a
BradleyGilden <bradleygilden@gmail.com>
2023-06-22 15:20:58 +0200

    new changes

--- main.c
+++ main.c
@@ -19,6 +19,29 @@ int main(int argc, char *argv[])
 	}
 	return (0);
 }
+
+void close_and_free(glo_t *global, stack_t *stack)
+{
+	free(global->get_line);
+	empty_stack(stack);
+	fclose(global->file_d);
+}
+
+void file_err(char *argv)
+{
+	fprintf(stderr, "Error: Can't open file %s\n", argv);
+	exit(EXIT_FAILURE);
+}
+
+void res_chck(char *itm, int cnt, glo_t *gb, char *ars, stack_t **stk, int res)
+{
+	res = opc_fun(stk, ars, itm, cnt);
+	if (res == 1)
+		push_err(gb->file_d, gb->get_line, *stk, cnt);
+	else if (res == 2)
+		no_cmd(gb->file_d, gb->get_line, *stk, ars, cnt);
+}
+
 /**
  * command_handler - Read file
  * @argv: Arguments
@@ -28,37 +51,28 @@ void command_handler(char *argv)
 {
 	int count = 0, result = 0;
 	size_t bufsize = 0;
-	char *arguments = NULL, *item = NULL;
+	char *args = NULL, *item = NULL;
 	stack_t *stack = NULL;
 
 	global.file_d = fopen(argv, "r");
 	if (global.file_d)
 	{
-		while (getline(&global.get_line, &bufsize, global.file_d) != -1)
+		for (; getline(&global.get_line, &bufsize, global.file_d) != -1;)
 		{
 			count++;
-			arguments = strtok(global.get_line, " \n\t\r");
-			if (arguments == NULL)
+			args = strtok(global.get_line, " \n\t\r");
+			if (args == NULL)
 			{
-				free(arguments);
+				free(args);
 				continue;
 			}
-			else if (*arguments == '#' || *arguments == '-')
+			else if (*args == '#' || *args == '-')
 				continue;
 			item = strtok(NULL, " \n\t\r");
-			result = opc_fun(&stack, arguments, item, count);
-			if (result == 1)
-				push_err(global.file_d, global.get_line, stack, count);
-			else if (result == 2)
-				no_cmd(global.file_d, global.get_line, stack, arguments, count);
+			res_chck(item, count, &global, args, &stack, result);
 		}
-		free(global.get_line);
-		empty_stack(stack);
-		fclose(global.file_d);
+		close_and_free(&global, stack);
 	}
 	else
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv);
-		exit(EXIT_FAILURE);
-	}
+		file_err(argv);
 }
